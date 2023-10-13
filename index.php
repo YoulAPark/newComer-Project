7e39556b6dbd869ab986506b9d29d4a99be25d00
youlapark <dbfdk2531@gmail.com>
2023-10-13 08:46:22 +0000

    index페이지 수정 메인에 링크걸어놓음

--- index.php
+++ index.php
@@ -5,5 +5,8 @@
 <div class="container mt-4 my-5 lead text-center">
     <div>
         <h1>메인페이지</h1>
+		<p>교육 내용과 관련한 페이지 생성</p>
+		<a href="/newcomer" class="nav-link px-2 text-secondary">메인페이지</a>
+		<a href="/newcomer/admin/register.php" class="nav-link px-2 text-secondary">회원가입페이지</a>
     </div>
 </div>
