9c61c6a3f4f87dd3855770047cf35d35ebb1aa78
youlapark <dbfdk2531@gmail.com>
2023-10-13 08:39:59 +0000

    회원가입 페이지 생성

--- admin/register.php
+++ admin/register.php
@@ -1,23 +1,35 @@
 <?php
 	include('../include/navbar.php');
 ?>
-<div class="container mt-4 my-5 lead text-center">
-    <h2>작성하기</h2>
 
-    <div>
-        <div class="mb-3 row">
-            <label class="col-sm-1 col-form-label">작성자</label>
-            <div class="col-sm-10">
-                <input type="text" readonly class="form-control-plaintext" value="닉네임">
-            </div>
-        </div>
-    </div>
-     
-    <div>
-        <div class="mb-3">
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="제목을 입력해주세요">
-            <label for="exampleFormControlTextarea1" class="form-label"></label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="내용을 입력해주세요"></textarea>
-        </div>
-    </div>
+<div class="container mt-4 my-5 lead">
+	<h2 style="font-weight: 700">회원가입</h2>
+	<div>
+		<p style="font-weight: 600">이메일주소<p>
+				<input type="email" class="form-control sm row " placeholder="사용하실 이메일주소를 입력해주세요">
+	</div>
+	<div>
+		<p style="font-weight: 600">닉네임<p>
+				<input type="text" class="form-control sm row" placeholder="사용하실 닉네임을 입력해주세요">
+	</div>
+	<div>
+		<p style="font-weight: 600">이름<p>
+				<input type="text" class="form-control sm row" placeholder="이름을 입력해주세요">
+	</div>
+	<div>
+		<div class="row">
+			<div class="col">
+				<p style="font-weight: 600">비밀번호</p>
+				<input type="password" class="form-control mb-3 row" placeholder="비밀번호를 입력해주세요">
+			</div>
+			<div class="col">
+				<p style="font-weight: 600">비밀번호확인</p>
+				<input type="password" class="form-control mb-3 row" placeholder="비밀번호를 다시 한번 입력해주세요">
+			</div>
+		</div>
+	</div>
+	<div class="d-grid gap-2 d-md-block">
+		<button class="btn btn-primary" type="button">가입하기</button>
+		<button class="btn btn-primary" type="button">뒤로가기</button>
+	</div>
 </div>
